1ae654bb1fa385669b9825d67dc62693a16aa6d1
Louis Austke <louis@austke.com>
2024-10-19 20:23:43 -0300

    whit sidebar darkened overlay

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // Grab the hamburger button and sidebar
 const toggleButton = document.getElementById("sidebar-toggle-btn")
 const sidebar = document.getElementById("sidebar")
+const overlay = document.getElementById("overlay")
 
 function showSidebar(visible) {
     // show (visible=true) or hide (visible=false) sidebar
     toggleButton.setAttribute("aria-expanded", visible)
     sidebar.setAttribute("aria-hidden", !visible)
+
+    if (visible) {
+        overlay.classList.add("darkened")
+    } else {
+        overlay.classList.remove("darkened")
+    }
 }
 window.showSidebar = showSidebar
 
@@ -23,6 +30,8 @@ toggleButton.addEventListener("click", toggleSidebar)
 
 // Function to update aria-hidden based on screen width
 function initSidebarAriaAttrs() {
+    overlay.classList.remove("darkened")
+
     if (window.innerWidth <= 768) {
         // On mobile, make the button accessible
         toggleButton.setAttribute("aria-hidden", "false")
@@ -41,3 +50,18 @@ function initSidebarAriaAttrs() {
 initSidebarAriaAttrs()
 // Update aria-hidden on window resize
 window.addEventListener("resize", initSidebarAriaAttrs)
+
+// Close the sidebar when it is open and the overlay is clicked
+overlay.addEventListener("click", function () {
+    if (sideBarIsOpen()) {
+        showSidebar(false)
+    }
+})
+
+// Close the sidebar when the Escape key is pressed
+window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && sideBarIsOpen()) {
+        // Check if the pressed key is Escape
+        showSidebar(false)
+    }
+})
